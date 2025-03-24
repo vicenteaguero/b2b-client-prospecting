a4885457b24576b86ee87d7c1f906102e18e5859
Vicente Agüero <vicenteaguero@uc.cl>
2025-03-24 00:19:01 -0300

    feat(prospecting): :sparkles: Add init file for library B2B Client Prospecting

---
+++ client_prospecting/__init__.py
@@ -0,0 +1,6 @@
+# client_prospecting/__init__.py
+
+from .email import get_gmail, get_unanswered_emails, get_email, send_email, get_thread_history
+from .prospecting import get_client, get_response, get_prompts
+
+
